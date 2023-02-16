60b0d8e77f39ea6a6d3bc6e9571f4f0d14cc499f
Darshan Shirke <Darshan.Shirke@ril.com>
2023-02-16 11:57:13 +0530

    properties file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,15 @@
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        String fullPath = args[0];
+        String parse = String.valueOf(parser.parse(new FileReader(fullPath)));
+        System.out.println("Hello World : String: " + parse);
     }
 }
