599a3d5849328671e5c685617103228c41fcbb80
Darshan Shirke <Darshan.Shirke@ril.com>
2023-02-16 12:16:58 +0530

    filereader

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,15 +2,22 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String fullPath = args[0];
-        String parse = null;
+        FileReader fileReader = null;
         try {
-            parse = String.valueOf(new FileReader(fullPath));
+            fileReader = new FileReader(fullPath);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
-        System.out.println("Hello World : String: " + parse);
+
+        int i;
+        while ((i = fileReader.read()) != -1) {
+            System.out.print((char) i);
+        }
+        fileReader.close();
+
+        System.out.println("Hello World : String: " + fileReader);
     }
 }
